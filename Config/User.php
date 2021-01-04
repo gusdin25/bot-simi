8463c7d6347d59ec3af42e76cfc60aa6f88d3a7a
gusdin25 <76903567+gusdin25@users.noreply.github.com>
2021-01-04 09:01:35 +0700

    Add files via upload

---
+++ Config/User.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Config;
+
+class User
+{
+    public $token = 'rqdxI-aFmVq-mxUwR-PYXfR';
+
+    public function getToken()
+    {
+        return $this->token;
+    }
+
+    public function setToken(string $token___)
+    {
+        return $this->token = $token___;
+    }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# Bot Chat
+
+Chat with bots.
+
+## How to install:
+
+```bash
+> pkg install php
+> git clone https://github.com/ryodevz/bot-chat
+> cd bot-chat
+```
+
+## Configuration:
+
+Set APi Token in `Config/User.php`
+
+## Run the tool:
+
+```bash
+> php index.php
+```
